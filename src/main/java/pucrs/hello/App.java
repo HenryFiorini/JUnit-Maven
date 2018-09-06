464c5f382aca9ad739f5f6fbfe8f163ed4f01ee4
Franco Henry Fiorini <franco.fiorini@acad.pucrs.br>
2018-09-06 16:27:08 -0300

    #1

---
+++ src/main/java/pucrs/hello/App.java
@@ -0,0 +1,17 @@
+package pucrs.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( getSaudacao() );
+    }
+
+	private static String getSaudacao() {
+		return "Hello World!";
+	}
+}
